0bec55fbae2e74134fd7389588b8cf0ee27dba66
PhuVinh206 <phuvinhyahoo@gmail.com>
2026-09-21 13:38:39 +0700

    Create auth.js

---
+++ auth.js
@@ -0,0 +1,14 @@
+function login(username, password) {
+  if (!username || !password) {
+    return false;
+  }
+  if (username === 'blocked_user') {
+    return false;
+  }
+  if (username === 'admin' && password === '123') {
+    return true;
+  }
+  return false;
+}
+
+module.exports = { login };
